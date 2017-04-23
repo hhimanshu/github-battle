40fc7f49a0c97e1a71381d0cfa3392365ea704d8
hhimanshu <harit.himanshu@yahoo.in>
2017-04-23 12:27:41 +1200

    Prepare for production

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
-var path = require('path');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
-module.exports = {
+
+const config = {
     entry: './app/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
@@ -21,3 +23,19 @@ module.exports = {
         template: 'app/index.html'
     })]
 };
+
+// preparing for production
+// In package.json: sets up the NODE_ENV in webpack.config
+// In here, NODE_ENV sets up the variable in compiled code for production
+if (process.env.NODE_ENV === 'production') {
+    config.plugins.push(
+        new webpack.DefinePlugin({
+            'process.env': {
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+        new webpack.optimize.UglifyJsPlugin()
+    )
+}
+
+module.exports = config;
